45de307f8c4ea2b2454d694ade5d63c15b8c1d14
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-08-14 08:13:58 +0000

    Fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -9,7 +9,7 @@ def fizzbuzz(n):
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
     - For multiples of three print "Fizz" instead of the number and for
-    multiples of five print "Buzz".
+      multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
@@ -21,7 +21,7 @@ def fizzbuzz(n):
             tmp_result.append("Fizz")
         elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0
+        elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
